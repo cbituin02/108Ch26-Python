7f9b66bf67b4a665178f99ede2ba998616c704b4
cbituin02 <97369678+cbituin02@users.noreply.github.com>
2022-03-21 00:50:24 -0700

    Create test.py

---
+++ Intro/test.py
@@ -0,0 +1,26 @@
+# this is a comment
+
+# variables
+name = "Caleb"
+last = "Bituin"
+age = 101
+found = False
+price = 19.99
+
+print(name + " " + last)
+
+print("hi im " + name + ", and my age is " + str(age) + "!")
+
+
+if age < 100:
+    print("dont worry you are still young :D")
+    print("Im still inside the if")
+    print("me too!")
+
+elif age == 100:
+    print("congrats you got to a century!")
+
+else:
+    print("sorry you are getting old!")
+
+# print("Im not :(")
