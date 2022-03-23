db2781c0ca89ffd393acb846c0044a0aa1af7f69
cbituin02 <97369678+cbituin02@users.noreply.github.com>
2022-03-22 19:40:06 -0700

    Assign 3

--- store_backend/server.py
+++ store_backend/server.py
@@ -1,12 +1,14 @@
-from flask import Flask
+import json
+from flask import Flask, abort
+from mock_data import catalog
+
 
 app = Flask("server")
+
 
 @app.route("/")
 def home():
     return "Hello from Flask"
-
-
 
 
 @app.route("/me")
@@ -14,4 +16,44 @@ def about_me():
     return "Caleb Bituin"
 
 
-app.run(debug=True)     
+#########################################################
+################### API ENDPOINTS #######################
+###################  RETURN JSON  #######################
+#########################################################
+
+
+@app.route("/api/catalog", methods=["get"])
+def get_catalog():
+    return json.dumps(catalog)
+
+
+@app.route("/api/catalog", methods=["post"])
+def save_product():
+    pass
+
+# GET /api/catalog/cpunt -> how many products exist in the catalog
+@app.route("/api/catalog/count", methods=["get"])
+def product_count():
+    return json.dumps(len(catalog))
+
+
+# get /api/catalog/total -> the sum of all the product's prices
+@app.route("/api/catalog/total")
+def catalogt_total():
+    total = 0
+    for prod in catalog:
+        total += prod["price"]
+
+    return json.dumps(total)
+
+#  /api/product/PATRICK
+@app.route("/api/product/<id>")
+def get_by_id(id):
+    # find the product with _id is eqaul to id
+    for prod in catalog:
+        if prod["_id"] == id:
+            return json.dumps(prod)
+
+    return abort(404, "no product with such id")
+
+app.run(debug=True)
---
+++ store_backend/test3.py
@@ -0,0 +1,20 @@
+from mock_data import catalog
+
+def lower_than(price):
+# print hjow many products exist
+# with a price lower than price var
+    pass
+
+
+def greater_than(price):
+    pass
+
+lower_than(10)
+lower_than(30)
+lower_than(50)
+lower_than(100)
+
+greater_than(10)
+greater_than(30)
+greater_than(50)
+greater_than(100)
